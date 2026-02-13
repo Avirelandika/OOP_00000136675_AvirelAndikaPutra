e06a27b370cc76229584f3cc88e4bd64a2739621
Mac <mac@Macs-MacBook-Air.local>
2026-02-13 15:30:02 +0700

    week02: (task 1) create Loan class with default args

---
+++ src/oop_00000136675_AvirelAndikaPutra/week02/Loan.kt
@@ -0,0 +1,14 @@
+class Loan(
+    val bookTitle: String,
+    val borrower: String,
+    val loanDuration: Int = 1 // Default Argument = 1 hari
+) {
+    // Method untuk menghitung denda
+    fun calculateFine(): Int {
+        return if (loanDuration > 3) {
+            (loanDuration - 3) * 2000
+        } else {
+            0
+        }
+    }
+}
