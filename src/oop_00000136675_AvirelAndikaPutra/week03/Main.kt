67a32742882e53663f0c6cdd6b797c65ee5bbc5a
Mac <mac@Macs-MacBook-Air.local>
2026-02-20 15:23:30 +0700

    week03: (task 2) implement Player testing in main

--- src/oop_00000136675_AvirelAndikaPutra/week03/Main.kt
+++ src/oop_00000136675_AvirelAndikaPutra/week03/Main.kt
@@ -3,26 +3,28 @@ package oop_00000136675_AvirelAndikaPutra.week03
 fun main() {
     val e = Employee("Budi")
 
-    println("--- Test Validasi Salary ---")
-    e.salary = -1000
+    println("--- Test Employee ---")
     e.salary = 5000000
-    println("Gaji: ${e.salary}")
-
     e.increasePerformance()
     e.printStatus()
-
-    println("Pajak yang harus dibayar: ${e.tax}")
-
+    println("Pajak: ${e.tax}")
 
     println("\n=== TESTING WEAPON (TASK 1) ===")
-
     val mySword = Weapon("Excalibur")
 
-    println("Action: Set damage ke -50")
     mySword.damage = -50
 
-    println("Action: Set damage ke 9999")
     mySword.damage = 9999
 
-    println("Hasil Akhir -> Nama: ${mySword.name} | Damage: ${mySword.damage} | Tier: ${mySword.tier}")
+    println("Senjata: ${mySword.name} | Damage: ${mySword.damage} | Tier: ${mySword.tier}")
+
+
+    println("\n=== TESTING PLAYER (TASK 2) ===")
+    val p1 = Player("JohnThor")
+
+    p1.addXp(50)
+    println("Status: ${p1.username} | Level: ${p1.level}")
+
+    p1.addXp(60)
+    println("Status: ${p1.username} | Level: ${p1.level}")
 }
