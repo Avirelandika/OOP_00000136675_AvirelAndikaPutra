518b987060d5ae0cdb6d57aff6b66bc676f729b3
Mac <mac@Macs-MacBook-Air.local>
2026-02-28 12:27:25 +0700

    week04:create basic vehicle parent class

---
+++ src/oop_00000136675_AvirelAndikaPutra/week04/Vehicle.kt
@@ -0,0 +1,13 @@
+package_oop_00000136675_AvirelAndikaPutra.week04
+
+open class Vehicle (val brand: string) {
+    var speed: int = 0
+    open fun accelerate() {
+        speed += 10
+        peintln("$brand melaju. kecepatan: $speed km/jam")
+    }
+
+    open fun honk() {
+        println("Beep beep!")
+    }
+}
