8828337a1b389226f9b34a982775fe38487f3638
Mac <mac@Macs-MacBook-Air.local>
2026-03-12 19:24:27 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_00000136675_AvirelAndikaPutra/week05/Mathhelper.kt
@@ -0,0 +1,7 @@
+package oop_00000136675_AvirelAndikaPutra.week05
+
+class MathHelper {
+    fun hitungLuas(sisi: Int) = sisi * sisi
+    fun hitungLuas(panjang: Int, lebar: Int) = panjang * lebar
+    fun hitungLuas(jariJari: Double) = 3.14 * jariJari * jariJari
+}
