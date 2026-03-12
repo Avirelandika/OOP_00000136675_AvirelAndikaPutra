a93d3dde053aa158b3814ad811ddc91eff794fc4
Mac <mac@Macs-MacBook-Air.local>
2026-03-12 19:23:36 +0700

    week05: implement polymorphic collection and smart casting

---
+++ src/oop_00000136675_AvirelAndikaPutra/week05/main.kt
@@ -0,0 +1,27 @@
+package oop_00000136675_AvirelAndikaPutra.week05
+
+fun main() {
+    val dosen1 = Dosen(nama = "Pak Alex", nidn = "0123456")
+    val admin1 = Admin(nama = "Bu Siti")
+
+    // Polymorphic Collection
+    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
+
+    println("=== AKTIVITAS PEGAWAI ===")
+    for (pegawai in daftarPegawai) {
+        pegawai.bekerja()
+
+        // Smart Casting dengan is dan when
+        when (pegawai) {
+            is Dosen -> {
+                println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
+                pegawai.mengajar()
+            }
+            is Admin -> {
+                println("=> Terdeteksi sebagai Admin")
+                pegawai.doAdminWork()
+            }
+        }
+        println("-------------------------")
+    }
+}
