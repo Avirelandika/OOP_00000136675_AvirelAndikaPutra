4d5835f67ce06f99047f64e47f1cec09cba3e3b2
Mac <mac@Macs-MacBook-Air.local>
2026-03-14 21:50:55 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000136675_AvirelAndikaPutra/week06/GadgetInterfaces.kt
@@ -0,0 +1,10 @@
+package oop_00000136675_AvirelAndikaPutra.week05
+
+// File: GadgetInterfaces.kt
+interface Camera {
+    fun turnOn() { println("Lensa kamera terbuka dan sensor aktif.") }
+}
+
+interface Phone {
+    fun turnOn() { println("Sinyal seluler mencari jaringan.") }
+}
