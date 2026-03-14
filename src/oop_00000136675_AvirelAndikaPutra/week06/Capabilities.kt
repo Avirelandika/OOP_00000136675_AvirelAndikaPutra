29601d1eba7c873e33bb8d5ba8504124dbb40e8d
Mac <mac@Macs-MacBook-Air.local>
2026-03-14 21:49:22 +0700

    week06: create compound interfaces

---
+++ src/oop_00000136675_AvirelAndikaPutra/week06/Capabilities.kt
@@ -0,0 +1,10 @@
+package oop_00000136675_AvirelAndikaPutra.week05
+
+// File: Capabilities.kt
+interface BluetoothConnectable {
+    fun connectToBluetooth()
+}
+
+interface Rechargeable {
+    fun chargeBattery()
+}
