b6dd4e608f3b35d9bd8fe7bfcb9c525ddbfc4d05
Mac <mac@Macs-MacBook-Air.local>
2026-03-14 21:51:36 +0700

    week06: (trial) trigger the diamond problem compiler error

---
+++ src/oop_00000136675_AvirelAndikaPutra/week06/Smartphone.kt
@@ -0,0 +1,7 @@
+package oop_00000136675_AvirelAndikaPutra.week05
+
+// File: Smartphone.kt
+// ERROR: Class Smartphone inherits multiple implementations of turnOn()
+class Smartphone : Camera, Phone {
+    // Biarkan error untuk sementara sesuai instruksi modul
+}
