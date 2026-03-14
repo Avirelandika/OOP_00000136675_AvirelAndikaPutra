8f139803a0f6f55685fd8de7e5076356e1bf541b
Mac <mac@Macs-MacBook-Air.local>
2026-03-14 21:58:50 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_00000136675_AvirelAndikaPutra/week06/SmartHomeHb.kt
@@ -0,0 +1,30 @@
+package oop_00000136675_AvirelAndikaPutra.week05
+
+// File: SmartHomeHub.kt
+class SmartHomeHub {
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+    }
+
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+
+    // Task Checkpoint 18: Smart Casting
+    fun activateSecurityMode() {
+        for (device in devices) {
+            if (device is Recordable) {
+                device.startRecord()
+            }
+            if (device is SmartSpeaker) {
+                device.playMusic("Sirine Peringatan")
+            }
+        }
+    }
+}
