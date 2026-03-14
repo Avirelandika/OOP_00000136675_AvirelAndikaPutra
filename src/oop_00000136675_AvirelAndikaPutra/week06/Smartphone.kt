88b438a0dc731632d296e507787a055f034c6855
Mac <mac@Macs-MacBook-Air.local>
2026-03-14 21:52:24 +0700

    week06: fix diamond problem using explicit super delegation

--- src/oop_00000136675_AvirelAndikaPutra/week06/Smartphone.kt
+++ src/oop_00000136675_AvirelAndikaPutra/week06/Smartphone.kt
@@ -1,7 +1,10 @@
 package oop_00000136675_AvirelAndikaPutra.week05
 
 // File: Smartphone.kt
-// ERROR: Class Smartphone inherits multiple implementations of turnOn()
 class Smartphone : Camera, Phone {
-    // Biarkan error untuk sementara sesuai instruksi modul
+    override fun turnOn() {
+        super<Camera>.turnOn() // Menjalankan logika Camera
+        super<Phone>.turnOn()  // Menjalankan logika Phone
+        println("Sistem operasi Smartphone berhasil booting.")
+    }
 }
