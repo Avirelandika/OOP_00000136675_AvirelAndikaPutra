0d5d70de7f4a6c3e7402ce7e0b61ac359898bb63
Mac <mac@Macs-MacBook-Air.local>
2026-03-14 21:55:07 +0700

    week06: implement decoupled processCheckout in main

---
+++ src/oop_00000136675_AvirelAndikaPutra/week06/Main.kt
@@ -0,0 +1,22 @@
+package oop_00000136675_AvirelAndikaPutra.week05
+
+// File: Main.kt
+fun processCheckout(method: PaymentMethod, amount: Double) {
+    println("-> Memulai checkout...")
+    method.pay(amount) // Dynamic polymorphism
+}
+
+fun main() {
+    val myWatch = Smartwatch()
+    myWatch.showTime()
+
+    val myPhone = Smartphone()
+    myPhone.turnOn()
+
+    val pay1 = Gopay()
+    val pay2 = CreditCard()
+
+    println("\n=== TESTING CHECKOUT ===")
+    processCheckout(pay1, 50000.0)
+    processCheckout(pay2, 150000.0)
+}
