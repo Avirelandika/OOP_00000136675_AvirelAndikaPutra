470868ff5f53f48ba326f2c86a514e8a151ff84e
Mac <mac@Macs-MacBook-Air.local>
2026-04-23 21:54:18 +0700

    week08: test safe casting in main

--- src/oop_00000136675_AvirelAndikaPutra/week08/Main.kt
+++ src/oop_00000136675_AvirelAndikaPutra/week08/Main.kt
@@ -24,4 +24,4 @@ fun main() {
 }
 val someObject: Any = 100
 val safeString = someObject as? String ?: "Unknown String"
-println("Hasil cast + fallback: $safeString")
+println("Hasil cast + fallback: $safeString")//
