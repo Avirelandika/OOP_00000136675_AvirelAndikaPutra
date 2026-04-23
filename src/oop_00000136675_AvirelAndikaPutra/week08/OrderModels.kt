a0060f0b9779972f9d365745539308b8dcad3df5
Mac <mac@Macs-MacBook-Air.local>
2026-04-23 21:49:09 +0700

    week08: create basic UserProfile with nullable fields

--- src/oop_00000136675_AvirelAndikaPutra/week08/OrderModels.kt
+++
@@ -1,6 +0,0 @@
-package oop_00000136675_avirel.andika.putra.week08
-
-class City(val name: String)
-class Address(val city: City?)
-class DeliveryDetails(val address: Address?)
-class Order(val deliveryDetails: DeliveryDetails?, val totalPrice: Int?)
