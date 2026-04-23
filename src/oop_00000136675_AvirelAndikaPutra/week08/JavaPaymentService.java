e100489ac3c4833e96240e5690b04d91a4293c64
Mac <mac@Macs-MacBook-Air.local>
2026-04-23 21:59:26 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000136675_AvirelAndikaPutra/week08/JavaPaymentService.java
@@ -0,0 +1,8 @@
+package oop_00000136675_avirel.andika.putra.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        // Simulasi return string dari sistem legacy
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
