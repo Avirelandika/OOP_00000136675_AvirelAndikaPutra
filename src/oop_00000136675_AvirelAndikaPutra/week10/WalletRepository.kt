9441b1d416ce09c2987fbc2110275b06ca91cf0b
Mac <mac@Macs-MacBook-Air.local>
2026-05-13 20:51:09 +0700

    week10: (task) implement generic WalletRepository

---
+++ src/oop_00000136675_AvirelAndikaPutra/week10/WalletRepository.kt
@@ -0,0 +1,18 @@
+class WalletRepository<T> {
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> {
+        return items
+    }
+
+    // Langkah 4: Filter dengan Constraints (Mencari berdasarkan nama jika T adalah Coin)
+    fun findByName(name: String): T? where T : Any {
+        return items.find {
+            if (it is Coin) it.name == name else false
+        }
+    }
+}
